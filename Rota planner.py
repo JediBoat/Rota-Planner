14d1c77101fded2f141b5c900c28ce9f2ea08b80
Jedidiah Boateng <121259910+JediBoat@users.noreply.github.com>
2026-07-17 19:20:52 +0100

    Add files via upload

---
+++ Rota planner.py
@@ -0,0 +1,20 @@
+import customtkinter 
+
+#main window
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+#Main menu 
+class MenuPage(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+# planning events
+class ExcelModePage(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+
+
+    
