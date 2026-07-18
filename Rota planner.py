56ae9e90deaff6bc6746b46eb16989b3593f8665
JediBoat <121259910+JediBoat@users.noreply.github.com>
2026-07-18 21:08:57 +0100

    Set up the main frame for widgets. The widgets will mainly be be buttons.

--- Rota planner.py
+++ Rota planner.py
@@ -17,7 +17,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("Rota Planner")
-        self.geometry("800x800")
+        self.geometry("1600x800")
 
         self.grid_rowconfigure(0, weight=1)#row 0 lots of space
         self.grid_columnconfigure(1, weight=1)#col 2 free space col 1 fixed
@@ -26,5 +26,15 @@ class App(customtkinter.CTk):
         self.sidebar = customtkinter.CTkFrame(self,corner_radius=0)
         self.sidebar.grid(row=0, column=0, sticky="nswe")
         self.sidebar.grid_rowconfigure(4, weight=1)
-        
-            
+
+        #main part of the grid
+        self.main = customtkinter.CTkFrame(self, width=1200, height=700, corner_radius=0)
+        self.main.grid(row=0, column=1)
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
